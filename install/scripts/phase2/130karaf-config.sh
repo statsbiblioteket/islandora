98025b234fe95c87b781f695ad1b5acf529a7dff
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-10-09 10:45:14 +0200

    More config install, possible not needed

--- install/scripts/phase2/130karaf-config.sh
+++ install/scripts/phase2/130karaf-config.sh
@@ -7,3 +7,5 @@ if [ -f "$HOME_DIR/islandora/install/configs/variables" ]; then
 fi
 
 cp "$HOME_DIR"/islandora/camel/sync/src/main/cfg/ca.islandora.sync.cfg /opt/karaf/etc
+cp "$HOME_DIR"/islandora/camel/services/basic-image-service/src/main/cfg/ca.islandora.services.basic.image.cfg /opt/karaf/etc
+cp "$HOME_DIR"/islandora/camel/services/collection-service/src/main/cfg/ca.islandora.services.collection.cfg /opt/karaf/etc
